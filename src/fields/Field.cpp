7c2e82a7e326340da14371ab97e7abaea372555c
Grigore Eugen-Andrei <grigoreandrey23@gmail.com>
2023-03-13 23:53:38 +0200

    implement base functions to throw

--- src/fields/Field.cpp
+++ src/fields/Field.cpp
@@ -1,5 +1,7 @@
 #include "Field.h"
 
+#include <stdexcept>
+
 std::ostream& operator<<(std::ostream& os, const Field& field) {
     return field.output(os);
 }
@@ -8,8 +10,18 @@ std::istream& operator>>(std::istream& is, const Field& field) {
     return field.input(is);
 }
 
-Field& Field::operator=(int data) { throw; }
-Field& Field::operator=(float data) { throw; }
-Field& Field::operator=(bool data) { throw; }
-Field& Field::operator=(const char* data) { throw; }
-Field& Field::operator=(const void* data) { throw; }
+Field& Field::operator=(int data) {
+    throw std::logic_error("Unimplemented int assigment operator");
+}
+Field& Field::operator=(float data) {
+    throw std::logic_error("Unimplemented float assigment operator");
+}
+Field& Field::operator=(bool data) {
+    throw std::logic_error("Unimplemented bool assigment operator");
+}
+Field& Field::operator=(const char* data) {
+    throw std::logic_error("Unimplemented string assigment operator");
+}
+Field& Field::operator=(const void* data) {
+    throw std::logic_error("Unimplemented void assigment operator");
+}
